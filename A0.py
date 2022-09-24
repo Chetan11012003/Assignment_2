0d477f8994a50b9b970733c633bdc0664a7b0789
Chetan11012003 <111963455+Chetan11012003@users.noreply.github.com>
2022-09-24 16:16:35 +0530

    Add files via upload

---
+++ A0.py
@@ -0,0 +1,6 @@
+''' create two python files A0.py and A1.py.Create a variable in A1.py and assign some value to it.
+write a python script to import A1 module in A0 and print value of the variables created in A0.py.
+'''
+
+import A1
+print(A1.x)
